19361b292090ce62e78d03d1f9f49e4bfaed05a1
Daniel Victoriano <djose1164@gmail.com>
2023-03-07 13:57:31 -0400

    add character component

---
+++ chapter-1/src/components/Character.js
@@ -0,0 +1,21 @@
+function Character(character) {
+    return (
+        <div className="col-3">
+            <div className="card">
+                <img
+                    src={character.image}
+                    alt={character.name}
+                    width="300"
+                    className="card-img-top"
+                />
+                <div className="card-body">
+                    <h3 className="card-title">{character.name}</h3>
+                    
+                    <p>{`origin: ${character.origin && character.origin.name}`}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Character;
