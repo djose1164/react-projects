0a5955c1737203f692c45e2504d97cd755d727f4
Daniel Victoriano <djose1164@gmail.com>
2023-03-07 13:57:05 -0400

    add index

---
+++ chapter-1/src/index.js
@@ -0,0 +1,19 @@
+import List from "./components/List.js";
+import ReactDOM from "react-dom/client"
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div className="container">
+            <nav className="navbar sticky-top navbar-light bg-dark">
+                <h1 className="navbar-brand text-light">Rick and Morty</h1>
+            </nav>
+            <List />
+        </div>
+    );
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
